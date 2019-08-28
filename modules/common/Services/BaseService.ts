15d4358209e3548f682ae6553bac5dab8160c28f
UnluckyAj <aashishjha.1994@gmail.com>
2019-08-28 17:50:51 +0530

    1 file modified

--- modules/common/Services/BaseService.ts
+++ modules/common/Services/BaseService.ts
@@ -1,24 +1,17 @@
-import { Observable, Subject } from 'rxjs'
+import { Observable, Subject, from } from 'rxjs'
 
 import Environment from '../../Environments/environment.dev';
 
 export default class BaseService {
   public webCall(url, method, body) : Observable<any>{
-    return  Observable.create((observer : Subject<any>) => {
-      fetch(Environment.apiUrl+ url , {
+    return from(fetch(Environment.apiUrl+ url , {
        method : method,
        headers : {
          Accept: 'application/json',
          'Content-Type': 'application/json',
          'Authorization' : "Bearer yy7MRECEM61sOlejFxJF1Ka8O6OdU7rCRtfh"
        }
-     }).then((res : Response) => res.json())
-     .then(data => {
-      observer.next(data);
-      observer.complete();
-     }).catch(err => {
-       observer.error(err);
-     });
-    });
+     })
+     .then((res : Response) => res.json()));
   }
 }
